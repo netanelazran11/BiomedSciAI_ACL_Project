b11308f0e67fdd43b6c723b0acd53e621b1d4c81
mattmadgwick <mattmadgwick@ibm.com>
2025-06-26 10:57:22 +0100

    add: evaluation and plotting helper functions

--- bmfm_targets/evaluation/__init__.py
+++ bmfm_targets/evaluation/__init__.py
@@ -1,6 +1,5 @@
 """Evaluation utilities for trained models."""
 
-
 from .embeddings import (
     evaluate_clusters,
     generate_clusters,
@@ -8,13 +7,36 @@ from .embeddings import (
     calculate_metrics_embedding,
 )
 from .clearml_utils import get_folder_of_checkpoints, get_model_path, get_root_folder
+from .utils import (
+    check_gpu,
+    convert_ids_to_label_names,
+    create_results_obs,
+    get_general_cell_type,
+    get_label_dict,
+    get_label_map,
+    merge_bmfm_adata,
+    plot_cell_type_counts,
+    plot_logits,
+    plot_embeddings,
+)
+
 
 __all__ = [
+    "calculate_metrics_embedding",
+    "check_gpu",
+    "convert_ids_to_label_names",
+    "create_results_obs",
     "evaluate_clusters",
     "generate_clusters",
     "get_folder_of_checkpoints",
+    "get_general_cell_type",
+    "get_label_dict",
+    "get_label_map",
     "get_model_path",
     "get_root_folder",
     "load_prediction_data_to_anndata",
-    "calculate_metrics_embedding",
+    "merge_bmfm_adata",
+    "plot_cell_type_counts",
+    "plot_logits",
+    "plot_embeddings",
 ]
