fa9a4590dc40ea35a0028eff2fcd448300af9418
netanelazran11 <netanelazran11@gmail.com>
2026-06-24 13:57:19 +0300

    Extend check_h5ad_uns to print uns values and layers

--- methyl/scripts/utils/check_h5ad_uns.py
+++ methyl/scripts/utils/check_h5ad_uns.py
@@ -1,11 +1,15 @@
 import h5py
+import numpy as np
 
 h5ad = "/sci/labs/benjamin.yakir/netanel.azran/data/data_methyl_pretrain_type3_h5ad/methylgpt_pretrain_type3.h5ad"
 with h5py.File(h5ad, "r") as f:
     print("Top-level keys:", list(f.keys()))
-    if "uns" in f:
-        print("uns keys:", list(f["uns"].keys()))
-    if "obsm" in f:
-        print("obsm keys:", list(f["obsm"].keys()))
-    if "obsp" in f:
-        print("obsp keys:", list(f["obsp"].keys()))
+    print("\n=== uns values ===")
+    for k in f["uns"].keys():
+        v = f["uns"][k]
+        try:
+            print(f"  uns['{k}']: {v[()]}")
+        except Exception:
+            print(f"  uns['{k}']: (group) keys={list(v.keys())}")
+    print("\n=== layers ===")
+    print("layer keys:", list(f["layers"].keys()))
